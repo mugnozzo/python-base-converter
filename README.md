16724ab2055b860f7ca77ab74a4230011b2775ea
Alek Mugnozzo <a.mugnani@hastega.it>
2023-01-09 22:39:04 +0100

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # python-number-base-converter
 A python program that will convert a given number from a given base to another given base.
+
+## Requirements
+- python 3.x;
+- Linux (not tesed on other operative systems).
+
+## Installation
+Just clone the repo where you want to put the folder. Navigate to the folder:
+```
+cd /path/to/folder
+```
+and clone the repo:
+```
+git clone https://github.com/mugnozzo/python-number-base-converter.git
+```
+A directory named `python-number-base-converter` will be created and the repo will be cloned into that directory.
+
+## Usage
+Navigate into the directory and exec the python script passing the arguments to it (see help for the complete list of arguments):
+```
+./pynbc.py -n <string_to_convert> -s <source_base> -d <destination_base>
+```
+
+## Missing features
+- Accepting a file (or stdin) as input;
+- check if inputs are well formatted (number respects given base and alphabet, etc...).
---
+++ pynbc.py
@@ -0,0 +1,51 @@
+#!/bin/python
+
+# Name:         Python Number Base Converter
+# Author:       Alek Mugnozzo
+# Version:      1.0.0
+# E-mail:       info@mugnozzo.net
+# Website:      mugnozzo.net
+# License:      GNU GPLv3.0
+# Hosted at:    https://github.com/mugnozzo/python-number-base-converter
+
+import argparse
+import sys
+
+aSou=['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+aDes=['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+
+argParser=argparse.ArgumentParser()
+argParser.add_argument("-n","--number",help="number to convert",required=True)
+argParser.add_argument("-s","--source-base",help="source base",required=True)
+argParser.add_argument("-d","--destination-base",help="destination base",required=True)
+
+args = argParser.parse_args()
+
+
+print("args=%s" % args)
+
+# TODO allow to pass file as input
+
+n=args.number
+s=int(args.source_base)         # TODO check if number fits source base
+d=int(args.destination_base)    # TODO check if destination fit maxbase
+
+n10=0
+i=0
+print(len(n))
+for c in n:
+    ind=aSou.index(c)
+    n10+=aSou.index(c)*(s**(len(n)-1-i))
+    i+=1
+
+print(n10)
+
+nd=""
+while(n10!=0):
+    nd+=aDes[n10%d]
+    n10=int(n10/d)
+
+print("Base "+str(s)+": "+str(n))
+print("Base "+str(d)+": "+nd[::-1])
+
+# TODO if the destination base is not given, calculate some default bases (2 8 10 16)
